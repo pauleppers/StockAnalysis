b73fbd6abdf4fee28f4fd13767bdb09bd654ba42
Juliann Pezzullo <jump8621@gmail.com>
2021-01-06 12:47:36 -0500

    nothing

--- New Folder/add_columns.sql
+++ New Folder/add_columns.sql
@@ -1,11 +1,22 @@
-ALTER TABLE hon
+ALTER TABLE ge
+ADD COLUMN cal integer
 ADD COLUMN rel Float,
-ADD COLUMN cal INTEGER,
 
 ALTER TABLE hon
 DROP COLUMN rel,
 DROP COLUMN cal,
 
 
-SELECT * FROM hon
+SELECT * FROM mdrx
 
+SELECT * 
+FROM ge
+FULL OUTER JOIN hon 
+	ON ge.symbol = hon.symbol
+	
+	
+	
+SELECT *
+FROM Employee
+     FULL OUTER JOIN Departments ON Employee.EmpID = Departments.EmpID;	
+	
